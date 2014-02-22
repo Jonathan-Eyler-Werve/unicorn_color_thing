52e2002a2d7872d6208803573e718ea6d796da1e
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2014-02-22 15:17:26 -0600

    Creep constructor!

--- creeps.js
+++ creeps.js
@@ -1,12 +1,38 @@
 <!--  Contains functions to create, draw and modify creeps -->
 
-    function drawCreep(ctx){
+
+var creeps = [];
+
+// creeps << my_creep;
+
+function creep(posX,posY,direction,hitPoints){
+	this.posX=posX;
+	this.posY=posY;
+	this.direction=direction;
+	this.hitPoints=hitPoints;
+	this.image = new Image();
+	this.image.src = 'images/icon_27452.svg';
+
+	this.drawCreep=drawCreep;
+	function drawCreep(ctx){
+		ctx.save();
+	  ctx.translate(this.posX, this.posY);
+	  ctx.rotate(to_rad(this.direction));
+		ctx.drawImage(this.image,-25,-25);
+	  ctx.restore();
+	};
+
+};
+
+function drawCreep(ctx){
+	ctx.save();
+  ctx.translate(this.posX, this.posY);
+  ctx.rotate(to_rad(this.direction));
+	ctx.drawImage(this.image,-25,-25);
+  ctx.restore();
+};
 
 
-    	ctx.save();
-      ctx.translate(creep.posX, creep.posY);
-      ctx.rotate(to_rad(creep.direction));
-    	ctx.drawImage(creep,0,0);
-      ctx.restore();
+var my_creep = new creep(100,100,180,10);
 
-    };
+console.log(my_creep)
