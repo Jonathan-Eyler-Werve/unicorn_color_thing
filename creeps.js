3767f014f1752a47415f9edfc93dd7e015d98a4d
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2014-02-22 16:29:50 -0600

    added Random Factor generators

--- creeps.js
+++ creeps.js
@@ -7,13 +7,16 @@ function creep(posX,posY,direction,hitPoints){
 	this.hitPoints=hitPoints;
 	this.image = new Image();
 	this.image.src = 'images/icon_27452.svg';
+	this.randomShort = 5
+	this.randomMedium = 5
+	this.randomLong = 5
 
 	this.drawCreep=drawCreep;
 	function drawCreep(ctx){
 		ctx.save();
 	  ctx.translate(this.posX, this.posY);
 	  ctx.rotate(to_rad(this.direction));
-		ctx.drawImage(this.image,-25,-25);
+		ctx.drawImage(this.image,-50,-50);
 	  ctx.restore();
 	};
 };
@@ -27,11 +30,39 @@ function drawCreep(ctx){
 };
 
 function updateCreeps(){
+
 	for (var i = 0; i < creeps.length; i++) {
-	    creeps[i].direction = creeps[i].direction + 1
-	    console.log(creeps[i].direction)
+	    creeps[i].direction = creeps[i].direction + creeps[i].randomMedium;
 	};
+
 };
+
+function updateRandomFactors(){
+
+		if (gameLoopCounter%30 == 1){	// SHORT random factor generator
+		console.log("%30")
+		for (var i = 0; i < creeps.length; i++) {
+		    creeps[i].randomLong = to_i(((Math.random() * 10) + 1));
+		    console.log(creeps[i].randomLong)
+		};
+	};
+
+	if (gameLoopCounter%100 == 1){	// MEDIUM random factor generator
+		console.log("%100")
+		for (var i = 0; i < creeps.length; i++) {
+		    creeps[i].randomMedium = to_i(((Math.random() * 10) + 1));
+		    console.log(creeps[i].randomMedium)
+		};
+	};
+
+	if (gameLoopCounter%1000 == 1){	// LONG random factor generator
+		console.log("%1000")
+		for (var i = 0; i < creeps.length; i++) {
+		    creeps[i].randomLong = to_i(((Math.random() * 10) + 1));
+		    console.log(creeps[i].randomLong)
+		};
+	};
+}
 
 var creeps = [];
 var my_creep = new creep(100,100,180,10);
