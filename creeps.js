fea041298bee3636fb41a379dbac371e87d77763
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2014-02-22 14:55:23 -0600

    Creep drawing started. WIP

--- creeps.js
+++ creeps.js
@@ -1,1 +1,12 @@
 <!--  Contains functions to create, draw and modify creeps -->
+
+    function drawCreep(ctx){
+
+
+    	ctx.save();
+      ctx.translate(creep.posX, creep.posY);
+      ctx.rotate(to_rad(creep.direction));
+    	ctx.drawImage(creep,0,0);
+      ctx.restore();
+
+    };
