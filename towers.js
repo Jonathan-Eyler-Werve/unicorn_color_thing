20e118c0e4c57edc5e21ce966d78f8538ce2f01e
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2014-02-26 11:16:13 -0600

    Basic tower factory

--- towers.js
+++ towers.js
@@ -2,7 +2,28 @@
 
 // Image paths
 
-var roundArrow = 'images/icon_5438.svg';
+var imgRoundArrow = 'images/icon_5438.svg';
 
 // 
 
+function tower(posX,posY,type){
+	this.posX=posX;
+	this.posY=posY;
+	this.direction= 0;
+	this.hitPoints=hitPoints;
+	this.size= 10;
+	this.image = new Image();
+	this.image.src = imgRoundArrow;
+	this.randomShort = 5;
+	this.randomMedium = 5;
+	this.randomLong = 5;
+
+	this.drawTower=drawTower;
+	function drawCreep(ctx){
+		ctx.save();
+	  ctx.translate(this.posX, this.posY);
+	  ctx.rotate(to_rad(this.direction));
+		ctx.drawImage(this.image,-50,-50);
+	  ctx.restore();
+	};
+};
