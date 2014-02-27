0ef43ebc06504e74dc3a172df4e434dc2231d859
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2014-02-27 10:28:37 -0600

    Created bullet.js

---
+++ bullet.js
@@ -0,0 +1,8 @@
+//<!--  Contains functions to create, draw and modify projectiles of all sorts -->
+
+// Image paths
+
+var imgBullet = 'images/icon_30254.svg'; // Bullet designed by Broquil broquil from the Noun Project
+
+// 
+
