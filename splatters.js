0cb4ad8319bb831ecc4156b91fa9b5976ada7527
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2014-03-06 14:28:31 -0600

    Random analog WIP

--- splatters.js
+++ splatters.js
@@ -1,7 +1,13 @@
 //<!--  Contains functions to splatter stuff onto overlay based on game events -->
 // 
 
+function splat(x, y, color, size) {
+	this.x = x;
+	this.y = y;
+	this.color = color;
+	this.size = (size * Math.random()); 
 
+};
 
 
 
@@ -27,5 +33,24 @@ function randomSaturate(color) {
 	return tinycolor.saturate(color, _number).toHexString();
 };
 
+function randomAnalog(color) {
 
+	var _colors = tinycolor.analogous(color, results = 6, slices = 30)
+	var _number = to_i((Math.random() * 6));
+	console.log(_number);
+	console.log(_colors[_number].toHexString());
+	return tinycolor.saturate(color, _number).toHexString();
 };
+
+function to_i(_value)  {
+  // uses Bitwise operator to convert value to int. Allegedly faster than Math methods.
+  return _value | 0
+};
+
+randomAnalog('green');
+randomAnalog('green');
+randomAnalog('green');
+randomAnalog('green');
+randomAnalog('green');
+randomAnalog('green');
+randomAnalog('green');
